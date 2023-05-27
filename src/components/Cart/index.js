1d0f91bebc76f52b9373ed8b1f2c9b1a53159075
sravankumar-samala <srravankumarr@gmail.com>
2023-05-27 12:24:34 +0000

    Initial Commit

---
+++ src/components/Cart/index.js
@@ -0,0 +1,18 @@
+// Write your JS code here
+import Header from '../Header'
+
+const Cart = () => (
+  <>
+    <Header />
+    <div className="container-page">
+      {/* <div className="page-image-container"> */}
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-cart-img.png "
+        alt="cart"
+      />
+      {/* </div> */}
+    </div>
+  </>
+)
+
+export default Cart
---
+++ src/components/Products/index.js
@@ -0,0 +1,18 @@
+// Write your JS code here
+import Header from '../Header'
+
+const Products = () => (
+  <>
+    <Header />
+    <div className="container-page">
+      {/* <div className="page-image-container"> */}
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-products-img.png "
+        alt="products"
+      />
+      {/* </div> */}
+    </div>
+  </>
+)
+
+export default Products
